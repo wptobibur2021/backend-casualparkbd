66ffd3b43aa06be9372e9bce082e758eb1876818
wptobibur2021 <wpdevelopment20@gmail.com>
2023-02-21 17:43:34 +0600

    Path Info Change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 // Midileware
 app.use(express.json());
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 // https://casualparkbd23.vercel.app/
 app.use(bodyParser.json());
 // Routes
